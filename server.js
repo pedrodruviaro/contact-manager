c62c29f731ad8433ca59f595ddc066fb4ae7a2ae
pedrodruviaro <pedrodruviaro@gmail.com>
2025-04-25 19:46:29 -0300

    add mongodb database and connection with mongoose

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 const express = require("express")
 const router = require("./routes/contact.routes")
 const errorHandler = require("./middlewares/error.middleware")
+const connectDb = require("./config/dbConnection")
 
 const app = express()
 const PORT = process.env.PORT
@@ -11,5 +12,6 @@ app.use("/api/contacts", router)
 app.use(errorHandler)
 
 app.listen(PORT, () => {
+  connectDb()
   console.log(`Server running on port ${PORT}`)
 })
